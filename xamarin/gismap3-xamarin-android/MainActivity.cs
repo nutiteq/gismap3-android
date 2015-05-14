48b21f7422c9f6426ea29d775b6c20ee46ee4168
Jaak Laineste <jaak@nutiteq.com>
2015-05-14 16:57:42 +0300

    add listener, vector base map

--- xamarin/gismap3-xamarin-android/MainActivity.cs
+++ xamarin/gismap3-xamarin-android/MainActivity.cs
@@ -17,40 +17,80 @@ using Nutiteq.Layers;
 using Nutiteq.WrappedCommons;
 using Nutiteq.Styles;
 using Nutiteq.Projections;
+using Nutiteq.VectorTiles;
 
 namespace HelloMap
 {
 	[Activity (Label = "Hellomap", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
+			
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
+			// force Java to load PROJ.4 library. Needed as we don't call it directly, but 
+			// OGR datasource reading may need it.
+
+			//Java.Lang.JavaSystem.LoadLibrary("proj");
+
 			// Register license
 			Nutiteq.Utils.Log.ShowError = true;
 			Nutiteq.Utils.Log.ShowWarn = true;
-			MapView.RegisterLicense("XTUMwQ0ZRQzdURnJKck9HYUdhT09VNGFSN3o3Nmg3UWhjQUlVTnV4TStMMk0vemhPMXUwUnBGRlhwbmFtTklFPQoKcHJvZHVjdHM9c2RrLXhhbWFyaW4taW9zLTMuKixzZGsteGFtYXJpbi1hbmRyb2lkLTMuKgpwYWNrYWdlTmFtZT1jb20ubnV0aXRlcS5oZWxsb21hcC54YW1hcmluCmJ1bmRsZUlkZW50aWZpZXI9Y29tLm51dGl0ZXEuaGVsbG9tYXAueGFtYXJpbgp3YXRlcm1hcms9bnV0aXRlcQp2YWxpZFVudGlsPTIwMTUtMDYtMDEKdXNlcktleT0yYTllOWY3NDYyY2VmNDgxYmUyYThjMTI2MWZlNmNiZAo", ApplicationContext);
+			//Nutiteq.Utils.Log.ShowDebug = true;
 
+			//var licenseOk = MapView.RegisterLicense("XTUMwQ0ZRQ0ZRc2FRYnZWNGhNNEkxbFhjY3IvbGRSdHNLd0lVQjM4S3ZtNG44ZnIxR0tnWS9PYkdWL2llYlNFPQoKcHJvZHVjdHM9c2RrLXhhbWFyaW4taW9zLTMuKixzZGsteGFtYXJpbi1hbmRyb2lkLTMuKixzZGstZ2lzZXh0ZW5zaW9uCnBhY2thZ2VOYW1lPWNvbS5udXRpdGVxLmhlbGxvbWFwLnhhbWFyaW4KYnVuZGxlSWRlbnRpZmllcj1jb20ubnV0aXRlcS5oZWxsb21hcC54YW1hcmluCndhdGVybWFyaz1udXRpdGVxCnZhbGlkVW50aWw9MjAxNS0wNi0wMQp1c2VyS2V5PTJhOWU5Zjc0NjJjZWY0ODFiZTJhOGMxMjYxZmU2Y2JkCg==", ApplicationContext);
+			//Log.Info ("License ok = " + licenseOk);
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 			var mapView = (MapView)FindViewById (Resource.Id.mapView);
 
-			// Create package manager folder (Platform-specific)
-			var packageFolder = new File (GetExternalFilesDir(null), "gismap_temp");
-			if (!(packageFolder.Mkdirs() || packageFolder.IsDirectory)) {
-				Log.Fatal("Could not create package folder!");
+			mapView.Zoom = 2;
+
+			mapView.Layers.Add(new RasterTileLayer(new HTTPTileDataSource(0,18,"http://ecn.t3.tiles.virtualearth.net/tiles/r{quadkey}.png?g=1&mkt=en-US&shading=hill&n=z")));
+
+			// Now can add vector map as layer
+			// define styling for vector map
+			UnsignedCharVector styleBytes = AssetUtils.LoadBytes("osmbright.zip");
+			MBVectorTileDecoder vectorTileDecoder = null;
+			if (styleBytes != null) {
+				// Create style set
+				var vectorTileStyleSet = new MBVectorTileStyleSet (styleBytes);
+				vectorTileDecoder = new MBVectorTileDecoder (vectorTileStyleSet);
+			} else {
+				Log.Error ("Failed to load style data");
 			}
 
+
+//			VectorTileLayer baseLayer = new NutiteqOnlineVectorTileLayer("osmbright.zip");
+
+		//	var dataSource = new NutiteqOnlineTileDataSource("nutiteq.osm");
+			var dataSource = new HTTPTileDataSource (0, 14, "http://up1.nutiteq.com/v1/nutiteq.mbstreets/{zoom}/{x}/{y}.vt?user_key=15cd9131072d6df68b8a54feda5b0496");
+
+			var baseLayer = new VectorTileLayer(dataSource, vectorTileDecoder);
+		//	mapView.Layers.Add(baseLayer);
+
+
 			// Copy bundled tile data to file system, so it can be imported by package manager
-			foreach (string fileName in new string[] { "bina_polyon.dbf", "bina_polyon.prj", "bina_polyon.shp", "bina_polyon.shx" }) {
-				string importPath = new File (GetExternalFilesDir (null), fileName).AbsolutePath;
-				using (var input = Assets.Open (fileName)) {
-					using (var output = new System.IO.FileStream (importPath, System.IO.FileMode.Create)) {
-						input.CopyTo (output);
+			var assetFiles = Assets.List("");
+			var dir = GetExternalFilesDir (null);
+			dir = new File("/sdcard/");
+
+			foreach (string fileName in assetFiles) {
+				string importPath = new File (dir, fileName).AbsolutePath;
+				try{
+					using (var input = Assets.Open (fileName)) {
+						using (var output = new System.IO.FileStream (importPath, System.IO.FileMode.Create)) {
+							input.CopyTo (output);
+						}
 					}
+				}catch(IOException){
+					Log.Info ("IOException " + fileName);
 				}
+
 			}
+
 
 			// Set base projection
 			EPSG3857 proj = new EPSG3857();
@@ -68,19 +108,29 @@ namespace HelloMap
 			StyleSelector styleSelector = styleSelectorBuilder.BuildSelector();
 
 			// Create data source. Use constructed style selector and copied shape file containing points.
-			string shpPath = new File (GetExternalFilesDir (null), "bina_polyon.shp").AbsolutePath;
+			string shpPath = new File (dir, "VagYta.tab").AbsolutePath;
 			OGRVectorDataSource ogrDataSource = new OGRVectorDataSource(proj, styleSelector, shpPath);
 			VectorLayer ogrLayer = new VectorLayer(ogrDataSource);
+		//	ogrLayer.VisibleZoomRange = new MapRange (18, 20);
 			mapView.Layers.Add(ogrLayer);
 
 			// Zoom in to the actual data
 			MapBounds bounds = ogrDataSource.DataExtent;
+			Log.Info ("data bounds: " + bounds.ToString());
 			Android.Util.DisplayMetrics displaymetrics = new Android.Util.DisplayMetrics();
 			WindowManager.DefaultDisplay.GetMetrics(displaymetrics);
 			int height = displaymetrics.HeightPixels;
 			int width = displaymetrics.WidthPixels;
 			ScreenBounds screenBounds = new ScreenBounds (new ScreenPos (0, 0), new ScreenPos (width, height));
 			mapView.MoveToFitBounds(bounds, screenBounds, false, 0.5f);
+
+			// Create overlay layer for markers
+			var dataSourceOverlay = new LocalVectorDataSource (proj);
+			var overlayLayer = new VectorLayer (dataSourceOverlay);
+			mapView.Layers.Add (overlayLayer);
+		
+			mapView.MapEventListener = new MapListener (dataSourceOverlay, mapView);
+
 		}
 	}
 }
---
+++ xamarin/gismap3-xamarin-android/MapListener.cs
@@ -0,0 +1,96 @@
+﻿using Nutiteq.Core;
+using Nutiteq.Ui;
+using Nutiteq.DataSources;
+using Nutiteq.VectorElements;
+using Nutiteq.Styles;
+using Nutiteq.WrappedCommons;
+using Nutiteq.Utils;
+using Nutiteq.Graphics;
+using System.Text;
+
+namespace HelloMap
+{
+
+	public class MapListener : MapEventListener
+	{
+		private LocalVectorDataSource _dataSource;
+		private BalloonPopup _oldClickLabel;
+		private IMapView _mapView;
+
+		public MapListener(LocalVectorDataSource dataSource, IMapView mapView)
+		{
+			_dataSource = dataSource;
+			_mapView = mapView;
+		}
+
+		public override void OnMapClicked (MapClickInfo mapClickInfo)
+		{
+			// Remove old click label
+			if (_oldClickLabel != null) {
+				_dataSource.Remove(_oldClickLabel);
+				_oldClickLabel = null;
+			}
+		}
+
+		public override void OnMapMoved()
+		{
+
+			var mapPos1 = _mapView.ScreenToMap(new ScreenPos(0,0));
+		//	var mapPos2 = _mapView.ScreenToMap(new ScreenPos(_mapView.Width,_mapView.Height));
+				
+			Log.Debug ("bounds: " + mapPos1.X + " "+ mapPos1.Y); 
+
+		}
+
+		public override void OnVectorElementClicked(VectorElementsClickInfo vectorElementsClickInfo)
+		{
+			// A note about iOS: DISABLE 'Optimize PNG files for iOS' option in iOS build settings,
+			// otherwise icons can not be loaded using AssetUtils/Bitmap constructor as Xamarin converts
+			// PNGs to unsupported custom format.
+
+			// Remove old click label
+			if (_oldClickLabel != null) {
+				_dataSource.Remove(_oldClickLabel);
+				_oldClickLabel = null;
+			}
+
+			var clickInfo = vectorElementsClickInfo.VectorElementClickInfos[0];
+
+			var styleBuilder = new BalloonPopupStyleBuilder();
+			// Configure simple style
+			styleBuilder.LeftMargins = new BalloonPopupMargins (0, 3, 0, 6);
+			styleBuilder.RightMargins = new BalloonPopupMargins (0, 3, 0, 6);
+
+			// Make sure this label is shown on top all other labels
+			styleBuilder.PlacementPriority = 10;
+
+			var vectorElement = clickInfo.VectorElement;
+//			var clickText = vectorElement.GetMetaDataElement("ClickText");
+
+			var stringMap = vectorElement.GetMetaData();
+			StringBuilder msgBuilder = new StringBuilder ();
+
+			if (stringMap.Count > 0) {
+				
+				foreach (string key in stringMap.Keys) {
+					string value = "";
+					var success = stringMap.TryGetValue(key, out value);
+
+					Log.Debug(""+key+" = "+value);
+					msgBuilder.Append (key + " = " + value + "\n");
+				}
+				msgBuilder.Remove (msgBuilder.Length-1, 1);
+			}
+			var clickText = msgBuilder.ToString();
+			var clickPopup = new BalloonPopup(clickInfo.ElementClickPos, 
+				styleBuilder.BuildStyle(),
+				"Attributes:", 
+				clickText);
+
+			_dataSource.Add(clickPopup);
+			_oldClickLabel = clickPopup;
+
+
+		}
+	}
+}
